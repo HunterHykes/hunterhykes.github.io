3b97d6d20db8ea63cd2410a1226d5434f42ae800
HunterHykes <hh81@zips.uakron.edu>
2020-02-01 20:00:29 -0500

    added a bunch!

---
+++ script.js
@@ -0,0 +1,6 @@
+/* For Date/Time */
+var dt = new Date();
+document.getElementById("datetime").innerHTML = dt.toLocaleString();
+
+/* For Weather Widget */
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
